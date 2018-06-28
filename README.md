58b525532b1db431e4e4505889f6f33180f503d6
Carlo1911 <carlo.alva@pucp.pe>
2018-06-27 23:01:23 -0500

    getting email

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # resume_script
 Resume script for pdf, docs, excel &amp; csv
 
+Parse:
+    -name
+    -email (x)
+    -phone
+    -location
+    -language
+    -skills
+    -experience
+    -about
+
 ## Install
     sudo apt-get install python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig libpulse-dev
 
--- utils.py
+++ utils.py
@@ -1,10 +1,37 @@
-import docx2txt
+import re
 import textract
 
+email_re = r"[\w\.-]+@[\w\.-]+\.\w+"
 
-text = docx2txt.process("test/Adrien CHOROT.docx")
-print("TEXT")
-print(text)
-text2 = textract.process("test/Adrien CHOROT.docx")
-print("TEXT 2")
-print(text2)
+docs = [
+    'test/Adrien CHOROT.docx',
+    'test/Aikaterini Vafeiadi.pdf',
+    'test/Akram Sherif.doc',
+    'test/Babacar-Ndiaye.pdf',
+    'test/CHAND FINAL RESUME (11).docx',
+    'test/Cisem-Demirci.pdf',
+    'test/CV (1).pdf',
+    'test/CV English ( Cover Letter         included).pdf',
+    'test/CV English Edcel Uylenbroeck.docx',
+    'test/CV_by_Helen_Mäeots.docx',
+    'test/CV_EN.pdf',
+    'test/cv_Resume.docx',
+    'test/cvantoniolima_(1).pdf',
+    'test/ElenaA.pdf',
+    'test/Ihab El Mortada-resume new.doc',
+    'test/Ivonne Blanco.rtf',
+    'test/Jaap Gerretse.pdf',
+    'test/Jose Daniel Andino Pereira.docx',
+    'test/Piotr Lemanski.docx',
+    'test/primary%3ADownload%2FEngin Kılıç.pdf',
+    'test/Stefanie Haanfler.pdf',
+    'test/vasileios  sakkas.docx'
+]
+
+for doc in docs:
+    text = textract.process(doc).decode('utf-8')
+    matches = re.findall(email_re, text, re.MULTILINE)
+    if matches:
+        print(matches[0])
+    else:
+        print("no email detected")
