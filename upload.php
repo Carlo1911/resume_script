b8f4d09437bf6274ac41ad1f77232f11030ef756
Carlo1911 <carlo.alva@pucp.pe>
2018-07-03 07:34:24 -0500

    upload.py

--- upload.php
+++ upload.php
@@ -8,12 +8,12 @@ if(isset($_POST["submit"])) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 
         $old_path = getcwd();
-    chdir('/home/carlo0071/resume_script');
-    $output = shell_exec('sudo python3.6 main.py -f "test/Adrien CHOROT.docx"');
-    chdir($old_path);
-
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        chdir('/home/carlo0071/resume_script');
+        $output = shell_exec('sudo python3.6 main.py -f "test/Adrien CHOROT.docx"');
+        chdir($old_path);
         print_r($output);
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        
     } else {
         print_r($_FILES);
         echo "Sorry, there was an error uploading your file.";
