b21e36e878c4a7ea92f8bd1dd8fbfb60f81e087a
Carlo1911 <carlo.alva@pucp.pe>
2018-07-03 02:26:52 -0500

    upload

--- upload.php
+++ upload.php
@@ -6,16 +6,20 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+
+        $old_path = getcwd();
+    chdir('/home/carlo0071/resume_script');
+    $output = shell_exec('sudo python3.6 main.py -f "test/Adrien CHOROT.docx"');
+    chdir($old_path);
+
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        print_r($output);
     } else {
         print_r($_FILES);
         echo "Sorry, there was an error uploading your file.";
     }
 
-    $old_path = getcwd();
-    chdir('/home/carlo0071/resume_script');
-    $output = shell_exec('sudo python3.6 main.py -f "test/Adrien CHOROT.docx"');
-    chdir($old_path);
+    
 }
 
 ?>
